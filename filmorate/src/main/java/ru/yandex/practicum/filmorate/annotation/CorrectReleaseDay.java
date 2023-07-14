4ea8518d6e28257164372e38391bcc82461f227c
Светлана <kulechko@mail.ru>
2023-07-14 22:58:55 +0300

    новая ветка1

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/annotation/CorrectReleaseDay.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/annotation/CorrectReleaseDay.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.annotation;
+
 import ru.yandex.practicum.filmorate.validator.FilmReleaseDateValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
-@Target({ElementType.METHOD, ElementType.FIELD,ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
         ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
