71ba26248d0fbae822bbad8db6a1bc82f620a3d5
Светлана <kulechko@mail.ru>
2023-07-14 15:28:23 +0300

    1

--- controllers-films-users/filmorate/src/main/java/ru/yandex/practicum/filmorate/exception/WrongIdException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class WrongIdException extends RuntimeException{
-    public WrongIdException(String message) {
-        super(message);
-    }
-}
