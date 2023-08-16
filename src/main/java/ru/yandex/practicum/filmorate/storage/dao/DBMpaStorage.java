e0ab278b1bd0d6b48b072205260a00a60b7e1e03
Светлана <kulechko@mail.ru>
2023-08-16 16:31:31 +0300

    ТЗ11

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/DBMpaStorage.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.storage.dao;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.storage.MpaStorage;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+
+@Component
+public class DBMpaStorage implements MpaStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    public DBMpaStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Collection<Mpa> getAllMpa() {
+        String sqlMpa = "select * from RATINGMPA";
+        return jdbcTemplate.query(sqlMpa, this::makeMpa);
+    }
+
+    @Override
+    public Mpa getMpaById(int mpaId) {
+        String sqlMpa = "select * from RATINGMPA where RATINGID = ?";
+        Mpa mpa;
+        try {
+            mpa = jdbcTemplate.queryForObject(sqlMpa, this::makeMpa, mpaId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NotFoundException("Возрастной рейтинг с идентификатором " +
+                    mpaId + " не зарегистрирован!");
+        }
+        return mpa;
+    }
+
+    private Mpa makeMpa(ResultSet resultSet, int rowNum) throws SQLException {
+        return new Mpa(resultSet.getInt("RatingID"),
+                resultSet.getString("Name"),
+                resultSet.getString("Description"));
+    }
+}
