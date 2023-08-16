712ef5b4c8a09e9f507f6acaeae3049874020d20
Светлана <kulechko@mail.ru>
2023-08-16 16:31:31 +0300

    ТЗ11

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@WebMvcTest(value = UserController.class)
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private UserController userController;
+
+    @MockBean
+    private UserService service;
+
+    @Test
+    void shouldReturn200whenGetUsers() throws Exception {
+        User user = new User(0,
+                "correct.email@mail.ru",
+                "correctlogin",
+                "Correct Name",
+                LocalDate.of(2002, 1, 1),
+                new ArrayList<>());
+        Mockito.when(userController.findAll()).thenReturn(Collections.singletonList(user));
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Collections.singletonList(user))));
+    }
+
+    @Test
+    void shouldReturn200whenPostCorrectUserData() throws Exception {
+        User user = new User(0,
+                "correct.email@mail.ru",
+                "correctlogin",
+                "Correct Name",
+                LocalDate.of(2002, 1, 1),
+                new ArrayList<>());
+        Mockito.when(userController.create(Mockito.any())).thenReturn(user);
+        mockMvc.perform(post("/users")
+                        .content(objectMapper.writeValueAsString(user))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(user)));
+
+    }
+}
